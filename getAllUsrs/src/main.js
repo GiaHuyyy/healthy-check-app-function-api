d6e805e8f45aa8b61a652aab0926938f82973240
Gia Huy <tovugiahuy@gmail.com>
2024-11-09 16:16:40 +0700

    Test methob get user

--- getAllUsrs/src/main.js
+++ getAllUsrs/src/main.js
@@ -1,30 +1,42 @@
 import { Client, Users } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
+  const endpoint = process.env.endpoint;
+  const projectId = process.env.projectId;
+  const databaseId = process.env.databaseId;
+  const userCollectionId = process.env.userCollectionId;
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
+    .setEndpoint(endpoint)
+    .setProject(projectId)
+    // .setKey(req.headers['x-appwrite-key'] ?? '');
   const users = new Users(client);
 
-  try {
-    const response = await users.list();
+  if(req.method === 'GET') {
+    try {
+      const response = await users.listDocuments(databaseId, userCollectionId);
+      return res.json(response.documents);
+    } catch(err) {
+      return res.json(err);
+    }
+  }
+  // try {
+  //   const response = await users.list();
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+  //   log(`Total users: ${response.total}`);
+  // } catch(err) {
+  //   error("Could not list users: " + err.message);
+  // }
 
   // The req object contains the request data
-  if (req.path === "/ping") {
+  // if (req.path === "/ping") {
     // Use res object to respond with text(), json(), or binary()
     // Don't forget to return a response!
-    return res.text("Pong");
-  }
+    // return res.text("Pong");
+  // }
 
   return res.json({
     motto: "Build like a team of hundreds_",
